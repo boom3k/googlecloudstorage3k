389750d242610513790c79cb7065fed80b0c5e04
Ramel Henderson <boomboom3k@gmail.com>
2022-06-17 19:37:16 -0400

    Rewrite

--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
-module github.com/boom3k/googlecloudstorage4go
+module github.com/boom3k/googlecloudstorage3k
 
-go 1.17
+go 1.18
 
-require cloud.google.com/go/storage v1.18.2
+require (
+	cloud.google.com/go/storage v1.18.2
+	google.golang.org/api v0.58.0
+)
 
 require (
 	cloud.google.com/go v0.97.0 // indirect
@@ -16,7 +19,6 @@ require (
 	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/api v0.58.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20211016002631-37fc39342514 // indirect
 	google.golang.org/grpc v1.40.0 // indirect
---
+++ googlecloudstorage3k.go
@@ -0,0 +1,110 @@
+package googlecloudstorage3k
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"fmt"
+	"google.golang.org/api/option"
+	"io/ioutil"
+	"log"
+	"mime"
+	"path"
+	"strings"
+	"time"
+)
+
+type GoogleCloudStorage3k struct {
+	Client     *storage.Client
+	AdminEmail string
+	Domain     string
+}
+
+func BuildGoogleCloudStorage3k(adminEmail string, serviceAccountKey []byte, ctx context.Context) *GoogleCloudStorage3k {
+	newClient, err := storage.NewClient(ctx, option.WithCredentialsJSON(serviceAccountKey))
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+	newCloudStorage3k := &GoogleCloudStorage3k{Client: newClient, AdminEmail: adminEmail, Domain: strings.Split(adminEmail, "@")[1]}
+	log.Printf("CloudStorage3k Ready @ --> [%v]\n", &newCloudStorage3k)
+	return newCloudStorage3k
+}
+
+func (receiver *GoogleCloudStorage3k) UploadData(bucketName, fileName string, data []byte, timeout int) (*BucketObject, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(timeout))
+	defer cancel()
+	bucketWriter := receiver.Client.Bucket(bucketName).Object(fileName).NewWriter(ctx)
+	bucketWriter.ContentType = mime.TypeByExtension(path.Ext(fileName))
+	defer bucketWriter.Close()
+	sizeUploaded, err := bucketWriter.Write(data)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	log.Printf("Uploaded %s[%s] to --> {%s}\n", fileName, ByteCountSI(int64(sizeUploaded)), bucketName)
+	return &BucketObject{Filename: fileName, BucketName: bucketName, Data: data}, nil
+}
+
+func (receiver *GoogleCloudStorage3k) UploadObject(object *BucketObject, timeout int) (*BucketObject, error) {
+	return receiver.UploadData(object.BucketName, object.Filename, object.Data, timeout)
+}
+
+func (receiver *GoogleCloudStorage3k) DownloadObject(bucketName, objectName string, timeout int) (*BucketObject, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(timeout))
+	defer cancel()
+
+	bucketReader, err := receiver.Client.Bucket(bucketName).Object(objectName).NewReader(ctx)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+	defer bucketReader.Close()
+
+	data, err := ioutil.ReadAll(bucketReader)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	return &BucketObject{
+			BucketName: bucketName,
+			Filename:   objectName,
+			Data:       data},
+		nil
+}
+
+type BucketObject struct {
+	BucketName string
+	Filename   string
+	Data       []byte
+}
+
+func NewBucketObject(bucketName, fileName string, data []byte) *BucketObject {
+	return &BucketObject{
+		BucketName: bucketName,
+		Filename:   fileName,
+		Data:       data,
+	}
+}
+
+func (receiver *BucketObject) Upload(gcs3k *GoogleCloudStorage3k, timeout int) {
+	receiver, err := gcs3k.UploadData(receiver.BucketName, receiver.Filename, receiver.Data, timeout)
+	if err != nil {
+		log.Println(err.Error())
+		panic(err)
+	}
+}
+
+func ByteCountSI(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%dB", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f%cB",
+		float64(b)/float64(div), "kMGTPE"[exp])
+}
